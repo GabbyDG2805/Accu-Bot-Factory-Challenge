5ec739cc1c4c5262231bd2fbd5aff55117fec8be
Gabriella Di Gregorio <gabby2805@gmail.com>
2023-09-19 13:58:21 +0100

    Database Migrations

---
+++ database/migrations/2023_09_19_124410_create_components_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComponentsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('components', function (Blueprint $table) {
+            $table->id();
+            $table->string('sku')->unique();
+            $table->text('description');
+            $table->string('category');
+            $table->decimal('weight', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('components');
+    }
+}
---
+++ database/migrations/2023_09_19_125135_create_component_orders_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComponentOrdersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('component_orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('component_id');
+            $table->integer('quantity');
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('component_id')->references('id')->on('components');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('component_orders');
+    }
+}

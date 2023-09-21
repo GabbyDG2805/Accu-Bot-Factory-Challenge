35351e2b2360d3fe719cad6a6221b7ae6d961c19
Gabriella Di Gregorio <gabby2805@gmail.com>
2023-09-21 16:38:23 +0100

    Weight calculation

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -18,8 +18,18 @@ class Order extends Model
         'updated_at',
     ];
 
+    // Define a many-to-many relationship with the Component model, using the 'component_orders' pivot table
     public function components()
     {
-        return $this->belongsToMany(Component::class)->withPivot('quantity');
+        return $this->belongsToMany(Component::class, 'component_orders')->withPivot('quantity');
+    }
+
+    // Calculate the total weight of all components associated with this order
+    public function calculateTotalWeight()
+    {
+        return $this->components->sum(function ($component) {
+            // Calculate the weight of each component in this order by multiplying weight by quantity
+            return $component->weight * $component->pivot->quantity;
+        });
     }
 }
